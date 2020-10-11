fd177ac1970e3e2094864aa690a9cec1f819c539
Sai Kumar <adepusai123@gmail.com>
2020-10-11 22:17:13 +0530

    if else terinary

--- lib/main.dart
+++ lib/main.dart
@@ -16,28 +16,31 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  static var questions = const [
+    {
+      "questionText": "What's your favorite color?",
+      "answers": ["White", "Red", "Black"]
+    },
+    {
+      "questionText": "What's your favorite animal?",
+      "answers": ["Dog", "Cat", "Lion"]
+    },
+    {
+      "questionText": "What's your favorite anime?",
+      "answers": ["Naruto", "Boruto", "DragonBall Z"]
+    }
+  ];
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
+    if (_questionIndex < questions.length) {
+      print('You have more questions');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        "questionText": "What's your favorite color?",
-        "answers": ["White", "Red", "Black"]
-      },
-      {
-        "questionText": "What's your favorite animal?",
-        "answers": ["Dog", "Cat", "Lion"]
-      },
-      {
-        "questionText": "What's your favorite anime?",
-        "answers": ["Naruto", "Boruto", "DragonBall Z"]
-      }
-    ];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
@@ -45,22 +48,27 @@ class _MyAppState extends State<MyApp> {
               backgroundColor: Colors.blueGrey,
             ),
             // we can use Row also if we want
-            body: Column(
-              children: <Widget>[
-                Question(questions[_questionIndex]['questionText']),
-                // passing pointer instead function to onPressed
-                ...(questions[_questionIndex]['answers'] as List<String>)
-                    .map((answer) {
-                  return Answer(_answerQuestion, answer);
-                }).toList(),
-                RaisedButton(
-                    child: Text("Clear State"),
-                    onPressed: () {
-                      setState(() {
-                        _questionIndex = 0;
-                      });
-                    }),
-              ],
-            )));
+            body: _questionIndex < questions.length
+                ? Column(
+                    children: <Widget>[
+                      Question(questions[_questionIndex]['questionText']),
+                      // passing pointer instead function to onPressed
+                      ...(questions[_questionIndex]['answers'] as List<String>)
+                          .map((answer) {
+                        return Answer(_answerQuestion, answer);
+                      }).toList(),
+                      RaisedButton(
+                          child: Text("Clear State"),
+                          onPressed: () {
+                            setState(() {
+                              _questionIndex = 0;
+                            });
+                          }),
+                    ],
+                  )
+                : Center(
+                    child: Text('You did it',
+                        style: TextStyle(color: Colors.redAccent)),
+                  )));
   }
 }
