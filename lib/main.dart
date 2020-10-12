bd5fb059e41548461742ea68a578bd304b20569f
Sai Kumar <adepusai123@gmail.com>
2020-10-12 10:21:31 +0530

    Splitted into Widgets Q & A & Results

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './answer.dart';
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  static var questions = const [
+  static var _questions = const [
     {
       "questionText": "What's your favorite color?",
       "answers": ["White", "Red", "Black"]
@@ -34,7 +34,7 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
-    if (_questionIndex < questions.length) {
+    if (_questionIndex < _questions.length) {
       print('You have more questions');
     }
   }
@@ -43,32 +43,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First App'),
-              backgroundColor: Colors.blueGrey,
-            ),
-            // we can use Row also if we want
-            body: _questionIndex < questions.length
-                ? Column(
-                    children: <Widget>[
-                      Question(questions[_questionIndex]['questionText']),
-                      // passing pointer instead function to onPressed
-                      ...(questions[_questionIndex]['answers'] as List<String>)
-                          .map((answer) {
-                        return Answer(_answerQuestion, answer);
-                      }).toList(),
-                      RaisedButton(
-                          child: Text("Clear State"),
-                          onPressed: () {
-                            setState(() {
-                              _questionIndex = 0;
-                            });
-                          }),
-                    ],
-                  )
-                : Center(
-                    child: Text('You did it',
-                        style: TextStyle(color: Colors.redAccent)),
-                  )));
+      appBar: AppBar(
+        title: Text('My First App'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      // we can use Row also if we want
+      body: _questionIndex < _questions.length
+          ? Quiz(
+              questions: _questions,
+              questionIndex: _questionIndex,
+              answerQuestion: _answerQuestion,
+            )
+          : Result(),
+    ));
   }
 }
