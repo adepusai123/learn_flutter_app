7da11232b250cd1bd5b092ce22784bd094c1a7eb
Sai Kumar <adepusai123@gmail.com>
2020-10-12 10:55:38 +0530

    get Method added

--- lib/result.dart
+++ lib/result.dart
@@ -4,6 +4,18 @@ class Result extends StatelessWidget {
   final int score;
   final Function press;
 
+  String get resultPhrase {
+    var resultText = 'You did it';
+    if (score <= 15 && score <= 20) {
+      resultText = "You are good and score is: ";
+    } else if (score >= 20 && score <= 30) {
+      resultText = "You are awesome and score is: ";
+    } else {
+      resultText = "You are so bad and score is: ";
+    }
+    return resultText;
+  }
+
   const Result({Key key, this.score, this.press}) : super(key: key);
   @override
   Widget build(BuildContext context) {
@@ -11,7 +23,7 @@ class Result extends StatelessWidget {
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text('Your score is : ',
+        Text(resultPhrase,
             style: TextStyle(color: Colors.redAccent, fontSize: 24)),
         Text('$score',
             style: TextStyle(
